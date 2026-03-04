966bf3487333d7cb82cd5504bfff7418537d4b08
Dogus <dogus.sunna@stud.hslu.ch>
2026-03-04 14:09:45 +0100

    Dashboard developed

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -2,5 +2,7 @@ public class Dashboard {
     public static void main(String[] args) {
         System.out.println("Dashboard loaded");
 
+        System.out.println("Dashboard developed");
+
     }
 }
