0f2c06046a18caa8e0cdc2690d955871b88ecd6c
iathambi <mathangi.thambirasa@stud.hslu.ch>
2026-04-20 18:35:39 +0200

    Remove README.md writing functionality from lead_time.py
    Removed functionality to write lead time statistics to README.md.

--- lead_time.py
+++ lead_time.py
@@ -30,11 +30,3 @@ if lead_times:
     avg = sum(lead_times) / len(lead_times)
     print(f"\nDurchschnitt: {avg:.2f} Stunden")
 
-with open("README.md", "w") as f:
-    f.write("# Github Projekt\n\n")
-    f.write("## PR Lead Time\n\n")
-    
-    if lead_times:
-        f.write(f"Durchschnitt: {avg:.2f} Stunden\n")
-    else:
-        f.write("Keine gemergten PRs gefunden\n")
